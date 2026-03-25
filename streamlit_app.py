dfb070e7d57d4eb4d48c7fab0f7868550012a6cd
Prateek Naik <97139507+Prateek6540@users.noreply.github.com>
2026-03-25 16:03:20 +0530

    Update API request URL to use search_on value

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,7 +53,7 @@ if ingrident_list:
         st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
       
         st.subheader(fruit_chosen + ' Nutrition Information')
-        smoothiefroot_response = requests.get(rf"https://my.smoothiefroot.com/api/fruit/{fruit_chosen}")  
+        smoothiefroot_response = requests.get(rf"https://my.smoothiefroot.com/api/fruit/{search_on}")  
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
     # st.write(ingredients_string)
 
