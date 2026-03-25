922d1865bce8270114eb697a7ed5c4e0a9514f28
Prateek Naik <97139507+Prateek6540@users.noreply.github.com>
2026-03-25 15:08:28 +0530

    Update Snowflake session handling in Streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 # Import python packages.
-import streamlit as st
-from snowflake.snowpark.context import get_active_session
+import streamlit as st  
 import snowflake.snowpark.functions as F 
 
 # Write directly to the app.
@@ -26,7 +25,8 @@ st.write(
 
 # st.write("Your favorite fruit is:", option)
 
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(F.col('FRUIT_NAME'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 
